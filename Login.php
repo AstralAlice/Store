31da431352ebc22aa7f81b78e0fab61f3c1bef7c
JB <B00128828@mytudublin.ie>
2021-04-19 23:18:27 +0100

    made some blank pages and started on basic forms

---
+++ Login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+</head>
+<body>
+<form action="Login.php" method="post">
+    <label for="username">Username</label>
+    <input type="text" placeholder="Type Here" name="username">
+
+    <label for="password">Password</label>
+    <input type="password" placeholder="Type Here" name="password">
+
+    <label for="submit">Submit</label>
+    <input type="submit" name="submit">
+
+
+</form>
+
+
+</body>
+</html>
